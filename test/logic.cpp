b511818b5b7352d66e36716b8da3cc26370b5089
Kaleb Barrett <dev.ktbarrett@gmail.com>
2020-06-11 23:12:28 -0500

    Add Logic tests

--- test/logic.cpp
+++ test/logic.cpp
@@ -4,71 +4,65 @@
 using namespace hdltypes;
 
 
-TEST_CASE("Logic constructors", "[logic]")
+TEST_CASE("Logic serialization", "[logic]")
 {
-    REQUIRE_NOTHROW( Logic() );
-    REQUIRE_NOTHROW( Logic(0) );
-    REQUIRE_NOTHROW( Logic(1) );
-    REQUIRE_NOTHROW( Logic(false) );
-    REQUIRE_NOTHROW( Logic(true) );
-    REQUIRE_NOTHROW( Logic('U') );
-    REQUIRE_NOTHROW( Logic('u') );
-    REQUIRE_NOTHROW( Logic('X') );
-    REQUIRE_NOTHROW( Logic('x') );
-    REQUIRE_NOTHROW( Logic('0') );
-    REQUIRE_NOTHROW( Logic('1') );
-    REQUIRE_NOTHROW( Logic('Z') );
-    REQUIRE_NOTHROW( Logic('z') );
-    REQUIRE_NOTHROW( Logic('W') );
-    REQUIRE_NOTHROW( Logic('w') );
-    REQUIRE_NOTHROW( Logic('L') );
-    REQUIRE_NOTHROW( Logic('l') );
-    REQUIRE_NOTHROW( Logic('H') );
-    REQUIRE_NOTHROW( Logic('h') );
-    REQUIRE_NOTHROW( Logic('-') );
-    REQUIRE_NOTHROW( Logic(Bit(0)) );
+    REQUIRE( Logic::deserialize('U').serialize() == 'U' );
+    REQUIRE( Logic::deserialize('u').serialize() == 'U' );
+    REQUIRE( Logic::deserialize('X').serialize() == 'X' );
+    REQUIRE( Logic::deserialize('x').serialize() == 'X' );
+    REQUIRE( Logic::deserialize('0').serialize() == '0' );
+    REQUIRE( Logic::deserialize('1').serialize() == '1' );
+    REQUIRE( Logic::deserialize('Z').serialize() == 'Z' );
+    REQUIRE( Logic::deserialize('z').serialize() == 'Z' );
+    REQUIRE( Logic::deserialize('W').serialize() == 'W' );
+    REQUIRE( Logic::deserialize('w').serialize() == 'W' );
+    REQUIRE( Logic::deserialize('L').serialize() == 'L' );
+    REQUIRE( Logic::deserialize('l').serialize() == 'L' );
+    REQUIRE( Logic::deserialize('H').serialize() == 'H' );
+    REQUIRE( Logic::deserialize('h').serialize() == 'H' );
+    REQUIRE( Logic::deserialize('-').serialize() == '-' );
 
-    REQUIRE_THROWS( Logic(6578) );
-    REQUIRE_THROWS( Logic('3') );
+    REQUIRE_THROWS( Logic::deserialize('8') );
+    REQUIRE_THROWS( Logic::deserialize('\0') );
 }
 
-TEST_CASE("Bit constructors", "[logic]")
+TEST_CASE("Logic conversions", "[logic]")
 {
-    REQUIRE_NOTHROW( Bit() );
-    REQUIRE_NOTHROW( Bit(0) );
-    REQUIRE_NOTHROW( Bit(1) );
-    REQUIRE_NOTHROW( Bit(false) );
-    REQUIRE_NOTHROW( Bit(true) );
-    REQUIRE_NOTHROW( Bit('0') );
-    REQUIRE_NOTHROW( Bit('1') );
-    REQUIRE_NOTHROW( Bit(Logic(0)) );
+    REQUIRE( to_int(to_logic(0)) == 0 );
+    REQUIRE( to_int(to_logic(1)) == 1 );
+
+    REQUIRE( to_bool(to_logic(false)) == false );
+    REQUIRE( to_bool(to_logic(true)) == true );
+
+    REQUIRE_THROWS( to_int('U'_l) );
+    REQUIRE_THROWS( to_bool('U'_l) );
+    REQUIRE_THROWS( to_logic(2) );
 }
 
-TEST_CASE("Logic bitops", "[logic]")
+TEST_CASE("Logic attributes", "[logic]")
 {
-    Logic a {'L'};
-    Logic b {1};
+    REQUIRE( is01('0'_l) );
+    REQUIRE_FALSE( is01('z'_l) );
 
-    REQUIRE( (a & b) == Logic(0) );
-    REQUIRE( (a | b) == Logic(1) );
-    REQUIRE( (a ^ b) == Logic(1) );
-    REQUIRE( ~a == Logic(1) );
+    REQUIRE( ('U'_l).value() == 0 );
+    REQUIRE( ('X'_l).value() == 1 );
+    REQUIRE( ('0'_l).value() == 2 );
+    REQUIRE( ('1'_l).value() == 3 );
+    REQUIRE( ('Z'_l).value() == 4 );
+    REQUIRE( ('W'_l).value() == 5 );
+    REQUIRE( ('L'_l).value() == 6 );
+    REQUIRE( ('H'_l).value() == 7 );
+    REQUIRE( ('-'_l).value() == 8 );
 }
 
-TEST_CASE("Bit bitops", "[logic]")
+TEST_CASE("Logic operations", "[logic]")
 {
-    Bit a {0};
-    Bit b {1};
+    REQUIRE( '0'_l == '0'_l );
+    REQUIRE( 'W'_l != 'X'_l );
 
-    REQUIRE( (a & b) == Bit(0) );
-    REQUIRE( (a | b) == Bit(1) );
-    REQUIRE( (a ^ b) == Bit(1) );
-    REQUIRE( ~a == Bit(1) );
-}
-
-TEST_CASE("Logic and Bit bitops automatic conversion", "[logic]")
-{
-    REQUIRE_NOTHROW( Bit(0) | '1' != 0 );
-    REQUIRE_NOTHROW( false ^ Logic(0) );
-    REQUIRE_NOTHROW( Bit('0') & Logic('Z') );
+    REQUIRE( ('0'_l & '1'_l) == '0'_l );
+    REQUIRE( ('0'_l | '1'_l) == '1'_l );
+    REQUIRE( ('X'_l ^ 'L'_l) == 'X'_l );
+    REQUIRE( ('W'_l & 'U'_l) == 'U'_l );
+    REQUIRE( ~'L'_l == '1'_l );
 }
