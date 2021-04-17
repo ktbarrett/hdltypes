770c1884499bfaf1f1e325c06ea3004f53d74a47
Kaleb Barrett <dev.ktbarrett@gmail.com>
2021-04-17 03:17:17 -0500

    Add support for cv-qualified types to is_integer_type

--- include/hdltypes/utils.hpp
+++ include/hdltypes/utils.hpp
@@ -1,22 +1,25 @@
 #ifndef HDLTYPES_UTIL_HPP
 #define HDLTYPES_UTIL_HPP
 
-#include <type_traits>  // false_type, true_type
+#include <type_traits>  // false_type, true_type, remove_cv
 
 namespace hdltypes { namespace util {
 
 
 /** User overloadable check to see if a type is a character type */
 template <typename T>
-struct is_char_type : std::false_type {};
+struct _is_char_type : std::false_type {};
 
-template <> struct is_char_type<char> : std::true_type {};
-template <> struct is_char_type<wchar_t> : std::true_type {};
-template <> struct is_char_type<char16_t> : std::true_type {};
-template <> struct is_char_type<char32_t> : std::true_type {};
+template <> struct _is_char_type<char> : std::true_type {};
+template <> struct _is_char_type<wchar_t> : std::true_type {};
+template <> struct _is_char_type<char16_t> : std::true_type {};
+template <> struct _is_char_type<char32_t> : std::true_type {};
 #if cplusplus >= 202000L
-template <> struct is_char_type<char8_t> : std::true_type {};
+template <> struct _is_char_type<char8_t> : std::true_type {};
 #endif
+
+template <typename T>
+struct is_char_type : _is_char_type<typename std::remove_cv<T>::type> {};
 
 
 /*
@@ -26,18 +29,21 @@ template <> struct is_char_type<char8_t> : std::true_type {};
 
 /** User overloadable check to see if a type is an integer type */
 template <typename T>
-struct is_integer_type : std::false_type {};
+struct _is_integer_type : std::false_type {};
 
-template <> struct is_integer_type<unsigned char> : std::true_type {};
-template <> struct is_integer_type<signed char> : std::true_type {};
-template <> struct is_integer_type<unsigned short> : std::true_type {};
-template <> struct is_integer_type<signed short> : std::true_type {};
-template <> struct is_integer_type<unsigned int> : std::true_type {};
-template <> struct is_integer_type<signed int> : std::true_type {};
-template <> struct is_integer_type<unsigned long> : std::true_type {};
-template <> struct is_integer_type<signed long> : std::true_type {};
-template <> struct is_integer_type<unsigned long long> : std::true_type {};
-template <> struct is_integer_type<signed long long> : std::true_type {};
+template <> struct _is_integer_type<unsigned char> : std::true_type {};
+template <> struct _is_integer_type<signed char> : std::true_type {};
+template <> struct _is_integer_type<unsigned short> : std::true_type {};
+template <> struct _is_integer_type<signed short> : std::true_type {};
+template <> struct _is_integer_type<unsigned int> : std::true_type {};
+template <> struct _is_integer_type<signed int> : std::true_type {};
+template <> struct _is_integer_type<unsigned long> : std::true_type {};
+template <> struct _is_integer_type<signed long> : std::true_type {};
+template <> struct _is_integer_type<unsigned long long> : std::true_type {};
+template <> struct _is_integer_type<signed long long> : std::true_type {};
+
+template <typename T>
+struct is_integer_type : _is_integer_type<typename std::remove_cv<T>::type> {};
 
 
 }}
